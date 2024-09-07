5f27d9d198e6c5651121bf51eb57d957d0f315b5
MyNameIsJeff-305 <michelgarcia950528@icloud.com>
2024-09-07 13:42:43 -0400

    Added and API Router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const usersRouter = require('./users.js');
+const sessionRouter = require('./session.js');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { validateSignup, handleValidationErrors } = require('../../utils/validations');
+const bcrypt = require('bcryptjs');
+const { User } = require('../../db/models');
+
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
+router.get('/api/csrf/restore', (_req, _res) => {
+    if (process.env.NODE_ENV !== 'production') {
+        router.get('/api/csrf/restore', (req, res) => {
+            res.cookie('XSRF-TOKEN', req.csrfToken());
+            return res.json({});
+        });
+    }
+});
+
+router.get('/require-auth', requireAuth, (req, res) => {
+    try {
+        return res.json(req.user);
+    } catch (error) {
+        next({
+            message: 'Error in /require-auth'
+        })
+    }
+});
+
+router.post('/signup', validateSignup, async (req, res, next) => {
+    // try block
+    try {
+        // deconstruct req.body
+        const defaultPassword = 'password'
+        const { email, password, username, firstName, lastName } = req.body;
+        // // create a hashed password for user
+        let hashedPassword;
+        if (password)
+            hashedPassword = bcrypt.hashSync(password);
+        else
+            hashedPassword = bcrypt.hashSync(defaultPassword)
+        // // create user record in Users table
+        const newUser = await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            username: username,
+            hashedPassword: hashedPassword
+        });
+        // create safeUser object for setTokenCookie function
+        const safeUser = {
+            id: newUser.id,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            email: newUser.email,
+            username: newUser.username
+        };
+        // set token cookie
+        await setTokenCookie(res, safeUser);
+
+        return res.json({
+            user: safeUser
+        });
+        // forward any errors not already sent
+    } catch (error) {
+        next(error);
+    };
+});
+
+module.exports = router;
