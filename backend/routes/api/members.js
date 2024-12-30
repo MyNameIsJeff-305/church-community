062d8e0a3509415dedc78f4d961324d596521601
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-12-30 08:03:38 -0500

    Delete a Member

--- backend/routes/api/members.js
+++ backend/routes/api/members.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { requireAuth, authorize } = require('../../utils/auth.js');
 
-const { Member, Gender, MemberType, CivilStatus } = require('../../db/models');
+const { Member, Gender, MemberType, CivilStatus, Household } = require('../../db/models');
 
 //Get all Members
 router.get('/', requireAuth, async (req, res, next) => {
@@ -50,6 +50,7 @@ router.get('/:id', requireAuth, async (req, res, next) => {
         const gender = await Gender.findByPk(member.genderId);
         const memberType = await MemberType.findByPk(member.memberTypeId);
         const civilStatus = await CivilStatus.findByPk(member.memberCivilStatusId);
+        const household = await Household.findByPk(member.householdId) || null;
 
         let safeMember = {
             id: member.id,
@@ -62,7 +63,8 @@ router.get('/:id', requireAuth, async (req, res, next) => {
             birthDate: member.birthDate,
             gender: gender.gender,
             memberType: memberType.memberType,
-            civilStatus: civilStatus.civilStatus
+            civilStatus: civilStatus.civilStatus,
+            household: household ? household.name : null
         }
 
         return res.json(safeMember);
@@ -76,8 +78,6 @@ router.get('/:id', requireAuth, async (req, res, next) => {
 router.post('/', requireAuth, async(req, res, next) => { //Implement Role and Permissions
     try {
         const { profileImg, firstName, lastName, alias, idNumber, birthDate, genderId, householdId, memberTypeId, memberStatusId, memberCivilStatusId } = req.body;
-
-        // console.log(firstName, lastName, alias, idNumber, birthDate, genderId, householdId, memberTypeId, memberStatusId, memberCivilStatusId, "THIS IS FIRSTNAME")
 
         const member = await Member.create({
             profileImg: profileImg || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
@@ -93,8 +93,6 @@ router.post('/', requireAuth, async(req, res, next) => { //Implement Role and Pe
             memberCivilStatusId
         });
 
-        console.log(member)
-
         return res.status(201).json(member);
     } catch (error) {
         
@@ -102,8 +100,54 @@ router.post('/', requireAuth, async(req, res, next) => { //Implement Role and Pe
 })
 
 //Update a Member
+router.put('/:id', requireAuth, async(req, res, next) => {
+    try {
+        const member = await Member.findByPk(req.params.id);
+
+        if (!member) {
+            return res.status(404).json({ message: 'Member not found' });
+        }
+
+        const { profileImg, firstName, lastName, alias, idNumber, birthDate, genderId, householdId, memberTypeId, memberStatusId, memberCivilStatusId } = req.body;
+
+        member.profileImg = profileImg || member.profileImg;
+        member.firstName = firstName || member.firstName;
+        member.lastName = lastName || member.lastName;
+        member.alias = alias || member.alias;
+        member.idNumber = idNumber || member.idNumber;
+        member.birthDate = birthDate || member.birthDate;
+        member.genderId = genderId || member.genderId;
+        member.householdId = householdId || member.householdId;
+        member.memberTypeId = memberTypeId || member.memberTypeId;
+        member.memberStatusId = memberStatusId || member.memberStatusId;
+        member.memberCivilStatusId = memberCivilStatusId || member.memberCivilStatusId;
+
+        await member.save();
+
+        return res.json(member);
+
+    } catch (error) {
+        next(error);
+    }
+});
 
 //Delete a Member
+router.delete('/:id', requireAuth, async(req, res, next) => {
+    try {
+        const member = await Member.findByPk(req.params.id);
+
+        if (!member) {
+            return res.status(404).json({ message: 'Member not found' });
+        }
+
+        await member.destroy();
+
+        return res.json({ message: 'Member deleted' });
+
+    } catch (error) {
+        next(error);
+    }
+});
 
 
 module.exports = router;
